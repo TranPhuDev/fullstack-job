3369556d3f966c81d2c4f407c2bda1df1b65df64
TranPhuDev <tranphudev3110@gmail.com>
2025-06-27 11:52:54 +0700

    module sub

--- BE/src/main/java/vn/tranphudev/jobhunter/domain/Subscriber.java
+++ BE/src/main/java/vn/tranphudev/jobhunter/domain/Subscriber.java
@@ -3,15 +3,17 @@ package vn.tranphudev.jobhunter.domain;
 import java.time.Instant;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
@@ -20,29 +22,29 @@ import lombok.Setter;
 import vn.tranphudev.jobhunter.util.error.SecurityUtil;
 
 @Entity
-@Table(name = "skills")
+@Table(name = "subscribers")
 @Getter
 @Setter
-public class Skill {
+public class Subscriber {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotBlank(message = "Tên kĩ năng không được để trống!")
+    @NotBlank(message = "email không được để trống")
+    private String email;
+
+    @NotBlank(message = "name không được để trống")
     private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = { "subscribers" })
+    @JoinTable(name = "subscriber_skill", joinColumns = @JoinColumn(name = "subscriber_id"), inverseJoinColumns = @JoinColumn(name = "skill_id"))
+    private List<Skill> skills;
 
     private Instant createdAt;
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
-
-    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "skills")
-    @JsonIgnore
-    private List<Job> jobs;
-
-    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "skills")
-    @JsonIgnore
-    private List<Subscriber> subscribers;
 
     @PrePersist
     public void handleBeforeCreate() {
@@ -61,4 +63,5 @@ public class Skill {
 
         this.updatedAt = Instant.now();
     }
+
 }
