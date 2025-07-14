f53712c3c61e8556e8c6f833497ed34e2836abb4
TranPhuDev <tranphudev3110@gmail.com>
2025-07-14 11:27:03 +0700

    add phone

--- FE-JOB/src/layout.tsx
+++ FE-JOB/src/layout.tsx
@@ -1,6 +1,21 @@
 import { Outlet } from 'react-router-dom'
 import AppHeader from 'components/layout/app.header'
 import AppFooter from './components/layout/app.footer'
+import { FiPhoneCall } from 'react-icons/fi';
+import './styles/phone-float.scss';
+
+
+function PhoneFloatingButton() {
+  return (
+    <a
+      href="tel:0774530086"
+      className="phone-float-btn"
+      title="Liên lạc"
+    >
+      <FiPhoneCall size={32} />
+    </a>
+  );
+}
 
 
 function Layout() {
@@ -10,6 +25,7 @@ function Layout() {
       <AppHeader />
       <Outlet />
       <AppFooter />
+      <PhoneFloatingButton />
     </>
   )
 }
